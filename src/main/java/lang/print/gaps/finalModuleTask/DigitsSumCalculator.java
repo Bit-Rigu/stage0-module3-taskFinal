28ed2f8fb140994eb12a2e1b3ec1f9731dd1b192
Dmitrii Bit-Rigu <daxudo@gmail.com>
2023-03-26 12:15:15 +0300

    Solving the tasks.

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,12 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+        int thousands = number / 1000;
+        int hundreds = (number - thousands * 1000) / 100;
+        int dozens = (number - thousands * 1000 - hundreds * 100) / 10;
+        int ones = number % 10;
+        int sum = thousands + hundreds + dozens + ones;
+        System.out.println(sum);
     }
+
 }
--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,11 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+        int hundreds = number / 100;
+        int dozens = (number - hundreds * 100) / 10;
+        int ones = number - hundreds * 100 - dozens * 10;
+        int reverseNumber = ones * 100 + dozens * 10 + hundreds;
+        System.out.println(reverseNumber);
     }
+
 }
